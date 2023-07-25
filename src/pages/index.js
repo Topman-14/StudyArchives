ab4ecc4973e5277944a9945035b9eca2f54750ab
Topman-14 <talktotimothy101@gmail.com>
2023-07-25 16:18:09 +0100

    update global styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
           <p className='text-sm text-center fade pb-4 mt-14'>Choose a faculty</p>
           <div id="heroBtns" className='flex justify-center gap-7 '>
           <LargeBtn href="https://drive.google.com/drive/folders/1CnuD0Of_iiJRiyP5vM798kKhCM3yuNpm" text="FBMAS" />
-          <LargeBtn href="https://drive.google.com/drive/folders/1CnuD0Of_iiJRiyP5vM798kKhCM3yuNpm" text="FMASS" />
+          <LargeBtn href="https://drive.google.com/drive/folders/1CnuD0Of_iiJRiyP5vM798kKhCM3yuNpm" text="FAMSS" />
           </div>
         <button className='float-right pt-3' onClick={scrollDown} >Learn More <KeyboardDoubleArrowDownIcon className='animate-bounce'/></button>
         </div>

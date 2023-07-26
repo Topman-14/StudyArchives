6d390d8728f08a8c97f5eda76b8d5bd27a64fa92
Topman-14 <talktotimothy101@gmail.com>
2023-07-26 12:30:19 +0100

    fix navbar bugs

--- src/pages/maintainers.js
+++ src/pages/maintainers.js
@@ -3,17 +3,17 @@ import Navbar from "../../components/Navbar";
 
 const Maintainers = () => {
     return ( 
-        <>
+        <div className="h-full">
             <Navbar />
             <h1 className="text-center text-3xl pt-10">Meet our Maintainers</h1>
-            <div className="flex gap-6 mx-auto mt-14 h-20 p-5 flex-wrap justify-center fade-up">
+            <div className="flex gap-6 mx-auto mt-10 p-8 flex-wrap justify-center fade-up">
                 <MaintainerTile img="" name="Emmanuel Tanimowo" github="" linkedin="" />
                 <MaintainerTile img="" name="Tope Akinkuade" github="" linkedin="" />
                 <MaintainerTile img="" name="Oreoluwa Favour" github="" linkedin="" />
                 <MaintainerTile img="" name="Joba Onakomaiya" github="" linkedin="" />
                 <MaintainerTile img="" name="Issac Odeh" github="" linkedin="" />
             </div>
-        </>
+        </div>
      );
 }
  

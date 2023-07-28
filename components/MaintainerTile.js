d55c8517159758de631e2673181171e65b9337d0
Topman-14 <talktotimothy101@gmail.com>
2023-07-28 20:08:03 +0100

    add maintainer images and links

--- components/MaintainerTile.js
+++ components/MaintainerTile.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import styles from './styles/MaintainerTile.module.css'
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import Link from 'next/link';
 
 export default function MaintainerTile(props) {
   return (
@@ -9,6 +12,10 @@ export default function MaintainerTile(props) {
             <img src={props.img} alt="" />
         </div>
         <h2 className='pt-20'>{props.name}</h2>
+        <div className='flex gap-5 mt-2'>
+          <Link href={props.github}><GitHubIcon /></Link>
+          <Link href={props.linkedin}><LinkedInIcon /></Link>
+        </div>
     </div>
   )
 }
--- src/pages/maintainers.js
+++ src/pages/maintainers.js
@@ -7,11 +7,11 @@ const Maintainers = () => {
             <Navbar />
             <h1 className="text-center text-3xl pt-10">Meet our Maintainers</h1>
             <div className="flex gap-6 mx-auto mt-10 p-8 flex-wrap justify-center fade-up max-w-4xl">
-                <MaintainerTile img="" name="Emmanuel Tanimowo" github="" linkedin="" />
-                <MaintainerTile img="" name="Tope Akinkuade" github="" linkedin="" />
-                <MaintainerTile img="" name="Oreoluwa Ojo" github="" linkedin="" />
-                <MaintainerTile img="" name="Joba Onakomaiya" github="" linkedin="" />
-                <MaintainerTile img="" name="Issac Odeh" github="" linkedin="" />
+                <MaintainerTile img="https://avatars.githubusercontent.com/u/68563757?v=4" name="Emmanuel Tanimowo" github="https://github.com/Mannuel25" linkedin="https://www.linkedin.com/in/emmanuel-tanimowo-6122291aa/" />
+                <MaintainerTile img="https://pbs.twimg.com/profile_images/1683872791924285440/cyZgvDES_400x400.jpg" name="Tope Akinkuade" github="https://github.com/Topman-14" linkedin="https://www.linkedin.com/in/tope-akinkuade/" />
+                <MaintainerTile img="https://media.licdn.com/dms/image/C4E03AQEp4bbQhTw7ng/profile-displayphoto-shrink_200_200/0/1648838979255?e=1695859200&v=beta&t=GKMJzL_pjknX3fyLsIZXJVKiHjTKn6FPQHyzSalPocM" name="Oreoluwa Ojo" github="https://github.com/Lloyd-the-god" linkedin="https://www.linkedin.com/in/favour-ojo-426795233/" />
+                <MaintainerTile img="" name="Joba Onakomaiya" github="https://github.com/JobaOnaks" linkedin="" />
+                <MaintainerTile img="" name="Issac Odeh" github="" linkedin="" ig="" />
             </div>
         </div>
      );

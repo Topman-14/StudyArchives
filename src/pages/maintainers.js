a2da8a2977e9d99fe102106036aedea3453f9cee
Favour Ojo <ojoore35@gmail.com>
2023-10-22 14:15:22 +0100

    fixed issue with maintenance page pictures

--- src/pages/maintainers.js
+++ src/pages/maintainers.js
@@ -7,7 +7,7 @@ const Maintainers = () => {
             <Navbar />
             <h1 className="text-center text-3xl pt-10">Meet our Maintainers</h1>
             <div className="flex gap-6 mx-auto mt-10 p-8 flex-wrap justify-center fade-up max-w-4xl">
-                <MaintainerTile img="https://avatars.githubusercontent.com/u/68563757?v=4" name="Emmanuel Tanimowo" github="https://github.com/Mannuel25" linkedin="https://www.linkedin.com/in/emmanuel-tanimowo-6122291aa/" />
+                <MaintainerTile img="/images/emma.jfif" name="Emmanuel Tanimowo" github="https://github.com/Mannuel25" linkedin="https://www.linkedin.com/in/emmanuel-tanimowo-6122291aa/" />
                 <MaintainerTile img="/images/tope.jpg" name="Tope Akinkuade" github="https://github.com/Topman-14" linkedin="https://www.linkedin.com/in/tope-akinkuade/" />
                 <MaintainerTile img="/images/ore.jfif" name="Oreoluwa Ojo" github="https://github.com/Lloyd-the-dev" linkedin="https://www.linkedin.com/in/favour-ojo-426795233/" />
                 <MaintainerTile img="/images/joba.jpg" name="Joba Onakomaiya" github="https://github.com/JobaOnaks" linkedin="" />
